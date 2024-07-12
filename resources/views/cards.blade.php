079ffe27039a8c42d88b2bad6a6bd06655834b80
giordanofabrizii <giordanofabrizi@gmail.com>
2024-07-12 15:38:21 +0200

    added style

--- resources/views/cards.blade.php
+++ resources/views/cards.blade.php
@@ -4,9 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel Cards</title>
+    @vite('resources/css/style.css')
+
+    <!-- Fontawesome 6.5.2 -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-    <a href="/">Return to Home Page</a>
-    <h1>QUESTE SONO LE CARD</h1>
+<header>
+        <div class="container">
+            <section>
+                <div class="logo">
+                    <img src="https://emoji.slack-edge.com/T91QPE3BP/pigiama_rick/d9c8ae806d94070a.jpg" alt="rick pigiama">
+                </div>
+                <div class="link">
+                    <a href="/">HOME</a>
+                    <a href="/forum">FORUM</a>
+                    <a href="/store">STORE</a>
+                </div>
+            </section>
+            <div class="profile">
+                <i class="fa-solid fa-shopping-cart"></i>
+                <i class="fa-solid fa-user"></i>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <h1>QUESTE SONO LE CARD</h1>
+        </div>
+    </main>
 </body>
 </html>
